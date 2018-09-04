6e80ec3053e99e8e55922c5122bbca046be88feb
tariq_tawalbeh <tawalbeh21@gmail.com>
2018-09-05 02:07:04 +0300

    First commit

---
+++ app/Http/Controllers/ajaxController.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ajaxController extends Controller
+{
+
+    /**
+     * getTwiterFeeds : function to get the most recent 30 tweets in UnitedNationsJO page 
+     * 
+     * @return json formatted array of latest tweets 
+     *
+     * @author Tariq Tawalbeh
+    */
+
+	public function getTwitterFeeds() {
+	     
+	    
+	    $url = 'https://twitter.com/Eng_TawTareq';
+
+	    
+	    $correctURLPattern = '|https?://(www\.)?twitter\.com/(#!/)?@?([^/]*)|';
+	    if (!preg_match($correctURLPattern, $url, $matches)) {
+	        throw new Exception('Not a valid URL');
+	    }
+	    //page name or page id
+	    $PageId = $matches[1];
+
+	    $PageId = 'UnitedNationsJO';
+	    $token = '1322735486-IwPcAxffIBhWeHZT1XldW3VqD4LNdGrUqMF3ewA';
+	        $token_secret = 'Quhyx9rtjiRB45NoGQmyWK8L6zq4Jxb3zHKxDHSMX5SWZ';
+	        $consumer_key = 'r5OdTex1cn0cez6x6dJWNAMgS';
+	        $consumer_secret = 'Ib9q2rj7CTPDLm2VPYPE27bYrciSoYoBnJmg9qv08geocffNDz';
+
+	        $host = 'api.twitter.com';
+	        $method = 'GET';
+	        $path = '/1.1/statuses/user_timeline.json'; 
+	       // api call path
+
+	        $query = array(// query parameters
+	            'screen_name' => $PageId,
+	            'count' => '50',
+	            'include_rts' => false
+	        );
+
+	        $oauth = array(
+	            'oauth_consumer_key' => $consumer_key,
+	            'oauth_token' => $token,
+	            'oauth_nonce' => (string) mt_rand(), 
+	            'oauth_timestamp' => time(),
+	            'oauth_signature_method' => 'HMAC-SHA1',
+	            'oauth_version' => '1.0'
+	        );
+
+	        $oauth = array_map("rawurlencode", $oauth); 
+	        $query = array_map("rawurlencode", $query);
+
+	        $arr = array_merge($oauth, $query); // combine the values THEN sort
+
+	        asort($arr); // secondary sort (value)
+	        ksort($arr); // primary sort (key)
+	        // http_build_query automatically encodes, but our parameters
+	        // are already encoded, and must be by this point, so we undo
+	        // the encoding step
+	        $querystring = urldecode(http_build_query($arr, '', '&'));
+
+	        $url = "https://$host$path";
+
+	        // gather everything together for the text to hash
+	        $base_string = $method . "&" . rawurlencode($url) . "&" . rawurlencode($querystring);
+
+	        // same with the key
+	        $key = rawurlencode($consumer_secret) . "&" . rawurlencode($token_secret);
+
+	        // generate the hash
+	        $signature = rawurlencode(base64_encode(hash_hmac('sha1', $base_string, $key, true)));
+
+        	// GET query 
+	        $url .= "?" . http_build_query($query);
+	        $url = str_replace("&amp;", "&", $url); 
+
+	        $oauth['oauth_signature'] = $signature; 
+	        ksort($oauth); // probably not necessary, but twitter's demo does it
+	        // also not necessary, but twitter's demo does this too
+
+	        function add_quotes($str) {
+	            return '"' . $str . '"';
+	        }
+
+	        // this is the full value of the Authorization line
+	        $auth = "OAuth " . urldecode(http_build_query($oauth, '', ', '));
+
+	        $options = array(CURLOPT_HTTPHEADER => array("Authorization: $auth"),
+	            CURLOPT_HEADER => false,
+	            CURLOPT_URL => $url,
+	            CURLOPT_RETURNTRANSFER => true,
+	            CURLOPT_SSL_VERIFYPEER => false);
+
+	        // final step to get the results 
+	        $feed = curl_init();
+	        curl_setopt_array($feed, $options);
+	        $json = curl_exec($feed);
+	        curl_close($feed);
+			echo $json;die;	    
+	}
+}
---
+++ resources/views/twitter_welcome.blade.php
@@ -0,0 +1,132 @@
+<head>
+    <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>    
+    <link href="//netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js"></script> 
+    
+</head>
+
+<body>
+    <div class="container">
+        <div class="row">
+         <h2>Twitter Tweets</h2>
+        <ul class="ds-btn">         
+            <li>
+                <a class="btn btn-lg btn-info" href="#" id="english_tweets">
+                <i class="glyphicon glyphicon-list pull-left"></i><span>English Tweets<br><small>@UnitedNationsJO</small></span></a> 
+            </li>
+            <li>
+                <a class="btn btn-lg btn-info" href="#" id="arabic_tweets">
+                <i class="glyphicon glyphicon-list pull-left"></i><span>Arabic Tweets<br><small>@UnitedNationsJO</small></span></a> 
+            </li>
+        </ul>
+        
+        </div>
+    </div>
+
+<div class="container mt-5 mb-5">
+    <div class="row">
+        <div class="col-md-6 offset-md-3" id="tweets">
+            
+        </div>
+    </div>
+</div>
+
+
+
+<style type="text/css">
+        .ds-btn li{ list-style:none; float:left; padding:10px; }
+        .ds-btn li a span{padding-left:15px;padding-right:5px;width:100%;display:inline-block; text-align:left;}
+        .ds-btn li a span small{width:100%; display:inline-block; text-align:left;}
+
+        ul.timeline {
+            list-style-type: none;
+            position: relative;
+        }
+        ul.timeline:before {
+            content: ' ';
+            background: #d4d9df;
+            display: inline-block;
+            position: absolute;
+            left: 29px;
+            width: 2px;
+            height: 100%;
+            z-index: 400;
+        }
+        ul.timeline > li {
+            margin: 20px 0;
+            padding-left: 20px;
+        }
+        ul.timeline > li:before {
+            content: ' ';
+            background: white;
+            display: inline-block;
+            position: absolute;
+            border-radius: 50%;
+            border: 3px solid #22c0e8;
+            left: 20px;
+            width: 20px;
+            height: 20px;
+            z-index: 400;
+        }        
+</style> 
+
+<script type="text/javascript">
+    $('#english_tweets').on('click', function (e) {
+    var months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+    e.preventDefault();
+    $.ajax({
+        type: "GET",
+        url: '/ajax/twitter',
+        success: function( output ) {
+            $("#tweets").empty();
+            var obj = JSON.parse(output);
+            $(obj).each(function( index, value){
+                if(value.lang == 'en'){
+                     var startTime = new Date(value.created_at);
+
+                    $("#tweets").append(
+                        '<ul class="timeline">'+
+                            '<li>'+
+                                '<a href="#" class="float-right">'+ months[startTime.getMonth()] + ', '+ startTime.getDay() + ' ' + startTime.getFullYear() +'</a>'+
+                                '<p>'+value.text+'</p>'+
+                            '</li>'+
+                        '</ul>'
+                    );
+                }
+            });
+                }
+        
+    });
+});
+
+    $('#arabic_tweets').on('click', function (e) {
+    var months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+    e.preventDefault();
+    $.ajax({
+        type: "GET",
+        url: '/ajax/twitter',
+        success: function( output ) {
+            $("#tweets").empty();
+            var obj = JSON.parse(output);
+            $(obj).each(function( index, value){
+                if(value.lang == 'ar'){
+                     var startTime = new Date(value.created_at);
+
+                    $("#tweets").append(
+                        '<ul class="timeline">'+
+                            '<li>'+
+                                '<a href="#" class="float-right">'+ months[startTime.getMonth()] + ', '+ startTime.getDay() + ' ' + startTime.getFullYear() +'</a>'+
+                                '<p>'+value.text+'</p>'+
+                            '</li>'+
+                        '</ul>'
+                    );
+                }
+            });
+                }
+        
+    });
+});
+
+</script>   
+</body>
+
